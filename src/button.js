3c6facde351b229f5157cbb792fde3e15f655a2d
Piña <ale.casp88@gmail.com>
2023-07-14 07:55:40 -0600

    Se agregaró la función de saveAlbum. Que guarda el contenido proporcionado por el usuario. Se dieron nuevos estilos a los albums y funcionlidades. Y se implementaron iconos SVG

--- src/button.js
+++ src/button.js
@@ -1,15 +1,20 @@
 const createButton = document.getElementById("createCardButton");
 const albumCreatorContainer = document.getElementById('albumCreatorContainer');
+const albumLibrary = document.getElementById('library');
 
-createButton.addEventListener('click', openAlbumCreator)
+createButton.addEventListener('click', openAlbumCreator);
+
 
 function openAlbumCreator() {
+
+
+    console.log("openAlbumCreator");
     albumCreatorContainer.innerHTML = `
-        <ul id='albumCreator' class="">
+        <ul id='albumCreator' class="overlay-album">
             <h2 class="title-h2 title-AC">Album Creator</h2>
             <li  class='AC-inputs-container'>
                 <p class="content">Nombre del album</p>
-                <input class="principal-input AC-input" type="text">
+                <input id='nameAlbumInput' class="principal-input AC-input" type="text">
             </li>
             <li class='AC-inputs-container'>
                 <p class="content ">Fondo del album</p>
@@ -22,15 +27,16 @@ function openAlbumCreator() {
                     <li class="select-color"></li>
                 </ul>
             </li>
-            <button id='doneAlbumCreatorButton' class="done-button button-AC">
-                <p class="content">Create</p>
-            </button>
+            <button id='doneAlbumCreatorButton' class="done-button button-AC">Create</button>
         </ul>
     `;
-    var albumCreator = document.getElementById("albumCreator");
+    const albumCreator = document.getElementById("albumCreator");
+    const doneAlbumCreatorButton = document.getElementById("doneAlbumCreatorButton");
+    const nameAlbumInput = document.getElementById("nameAlbumInput");
 
-    albumCreator.classList.add('overlay-album')
-    albumCreatorContainer.classList.add('overlay-bkg')
+    albumCreatorContainer.classList.add('overlay-bkg');
+
+    assignEventListeners();
 }
 
 
@@ -39,12 +45,41 @@ albumCreatorContainer.addEventListener("click", closeOverlay);
 
 function closeOverlay(event) {
     if (event.target === albumCreatorContainer) {
-        albumCreator.classList.add('move-out')
+        albumCreator.classList.add('move-out');
         
         setTimeout(() => {
             albumCreatorContainer.classList.remove('overlay-bkg');
-            albumCreator.classList.remove('move-out')
-            albumCreatorContainer.innerHTML = ``;
+            albumCreator.classList.remove('move-out');
+            albumCreatorContainer.innerHTML = '';
+            
         }, 350);
     }
 }
+function saveAlbum(){
+    let nameAlbumInput_Value = nameAlbumInput.value;
+    var newAlbumTemplate = `
+        <li class="album">
+            <a href="./src/album.html"></a>
+            <h4 class="title-h2">${nameAlbumInput_Value}</h4>
+            <img src="SVG/edit-B.svg" alt="edit-icon">
+        </li>
+    `;
+    albumLibrary.innerHTML += newAlbumTemplate;
+
+    albumCreator.classList.add('move-out');
+        
+    setTimeout(() => {
+        albumCreatorContainer.classList.remove('overlay-bkg');
+        albumCreator.classList.remove('move-out');
+        albumCreatorContainer.innerHTML= '';
+    }, 350);
+    const createButton = document.getElementById("createCardButton");
+
+    createButton.addEventListener('click', openAlbumCreator);
+
+}
+function assignEventListeners() {
+    doneAlbumCreatorButton.addEventListener("click", saveAlbum);
+    albumCreatorContainer.addEventListener("click", closeOverlay);
+}
+
