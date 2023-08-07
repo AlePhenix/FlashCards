ebbfd94ba0c301740893e10fe04c205f26840403
Piña <ale.casp88@gmail.com>
2023-08-06 20:37:53 -0600

    Arrays en donde se guardaran los datos de albums. Se agregaron funcionalidades de los formularios. Logica para evitar bugs de abrir una pagina o retroseder. Funciones de las Flash Cards de correcta o incorrecta en botones. Restructuracion del Header del album: Reacomodo del contador de cartas del album.

--- src/button.js
+++ src/button.js
@@ -7,15 +7,12 @@ const albumSlide = document.getElementById('AlbumSlide');
 function assignEventListeners() {
     doneAlbumCreatorButton.addEventListener("click", saveAlbum);
     albumCreatorContainer.addEventListener("click", closeOverlay);
-}
-
-function closeOverlayTransition(){
-    albumCreator.classList.add('move-out'); 
-    setTimeout(() => {
-        albumCreatorContainer.classList.remove('overlay-bkg');
-        albumCreator.classList.remove('move-out');
-        albumCreatorContainer.innerHTML= '';
-    }, 350);
+    nameAlbumInput.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            doneAlbumCreatorButton.click(); // Simula el clic en el botón
+        }
+    });
 }
 function openAlbumCreator() {
     albumCreatorContainer.innerHTML = `
@@ -42,6 +39,8 @@ function openAlbumCreator() {
     const albumCreator = document.getElementById("albumCreator");
     const doneAlbumCreatorButton = document.getElementById("doneAlbumCreatorButton");
     const nameAlbumInput = document.getElementById("nameAlbumInput");
+    
+    nameAlbumInput.focus();
     albumCreatorContainer.classList.add('overlay-bkg');
     assignEventListeners();
 }
@@ -52,101 +51,258 @@ function closeOverlay(event) {
         closeOverlayTransition();
     }
 }
+function closeOverlayTransition(){
+    albumCreator.classList.add('move-out'); 
+    setTimeout(() => {
+        albumCreatorContainer.classList.remove('overlay-bkg');
+        albumCreator.classList.remove('move-out');
+        albumCreatorContainer.innerHTML= '';
+    }, 350);
+}
 function saveAlbum(){
+    var counter = 0;
+    var arrayCards = [];
+    var wrongCards = [];
+    var rightCards = [];
+
+
+
+    function counterPlus (){
+        counter++;
+        const albumCounter = document.getElementById("albumCounter");
+        const cardsCounter = document.getElementById("cardsCounter");
+        const cardsMakerCounter = document.getElementById("cardsMakerCounter");
+        albumCounter.textContent = counter;
+        cardsCounter.textContent = counter;
+        cardsMakerCounter.textContent = counter;
+    }
+    function asignCardsTemplate(){
+        const flashCardsContainer = document.getElementById("FlashCardsContainer");
+        flashCardsContainer.innerHTML =  `
+        <div id="FlashCard" class="flashCard">
+            <p id="FlashCardContent" class="title-h2">${arrayCards[0].question}</p>
+            <img id="slackIcon" src="./SVG/slack.svg" alt="slack">
+        </div>  
+        <button id="ButtonWrong" class="WoRbutton buttonWrong">X</button>
+        <button id="ButtonRight" class="WoRbutton buttonRight">V</button>
+        `;
+        
+    //  EVENT LISTENERS
+
+        const backArrowIcon_Album = document.getElementById("BackArrowIcon_Album");
+        backArrowIcon_Album.addEventListener("click", () => {albumSlide.innerHTML = ""});
+
+
+        const slackIcon = document.getElementById("slackIcon");
+        const ButtonWrong = document.getElementById("ButtonWrong");
+        const ButtonRight = document.getElementById("ButtonRight");
+        slackIcon.addEventListener("click", getTheAnswer);
+        ButtonWrong.addEventListener("click", wrongAnswer);
+        ButtonRight.addEventListener("click", rightAnswer);
+
+
+        let answerCounter = 0;
+                    
+        const FlashCard = document.getElementById("FlashCard");
+        const flashCardContent = document.getElementById("FlashCardContent");
+
+    // Funciones de las Flash Cards
+        function getTheAnswer() {       
+            if (answerCounter%2 == 0) {
+              flashCardContent.textContent = arrayCards[0].answer;
+            } else {
+                flashCardContent.textContent = arrayCards[0].question;
+            }
+            FlashCard.classList.toggle("answer");
+            answerCounter++;
+        }
+        function wrongAnswer(){
+            FlashCard.classList.add("wrongAnswer");
+            wrongCards.push(arrayCards[0]);
+
+        }
+        function rightAnswer(){
+            FlashCard.classList.add("rightAnswer");
+            rightCards.push(arrayCards[0]);
+            console.log(rightCards);
+            rightCards.push("Si funciona");
+            console.log(rightCards);
+        }
+    }
+
     var nameAlbumInput_Value = nameAlbumInput.value;
-    const newAlbumTemplate = `
-        <li class="album">
-            <h4 id="albumName" class="title-h2">${nameAlbumInput_Value}</h4>
-            <img class="editIcon" src="SVG/edit-B.svg" alt="edit-icon">
-            <p class="title-h2">0/0</p>
-        </li>
-    `;
-    albumLibrary.innerHTML += newAlbumTemplate;
+
+    albumLibrary.insertAdjacentHTML("afterbegin", `
+    <li class="album">
+        <h4 id="albumName" class="title-h2">${nameAlbumInput_Value}</h4>
+        <p id="albumCounter" class="albumCounter">0/${counter}</p>
+    </li>
+    `); 
 
     closeOverlayTransition();
 
-    const createButton = document.getElementById("createCardButton");
-    createButton.addEventListener('click', openAlbumCreator);
-
     const album = document.querySelector(".album");
     album.addEventListener("click", openAlbum);
-}
 
-function openAlbum(event){
-    const editIcon = document.querySelector(".editIcon");
-    const albumName = document.getElementById("albumName");
-
-    var albumNameValue = albumName.textContent;
-
-    if(event.target !== editIcon){
-        console.log("Abrindo album");
-
-        function assignEvLis(){
-            const backArrowIcon_Album = document.getElementById("BackArrowIcon_Album");
-            const newFlashCardButton = document.getElementById("NewFlashCardButton");
-            backArrowIcon_Album.addEventListener("click", goBackToMain);
-            newFlashCardButton.addEventListener("click", createFlashCards);    
-        }
-
-        const openAlbumTemplate =`
-        <div class="albumPage">
-            <div class="albumSlideHeader">
-                <img id="BackArrowIcon_Album" class="backArrowIcon" src="./SVG/chevron-left-gray.svg" alt="back arrow">
-                <h3 class="title-h2 titleAlbumSLide">${albumNameValue}</h3>
-                <p id="cardsCounter" class="title-h2 cardsCounter">0/0</p>
-            </div>
-            <article class="flashCardsContainer">
-                <div id="NewFlashCardButton" class="newFlashCardButton">
-                    <p class="subtitle">Crear cartas</p>
-                    <img src="./SVG/pus-Icon.svg" alt="plus icon">
-                </div>
-            </article>
-        </div>
-        `;
-        albumSlide.innerHTML = openAlbumTemplate;
-        assignEvLis()
-
-        function goBackToMain(){
-            console.log("Cerrando album");
-            albumSlide.innerHTML = "";
-        } 
-        function createFlashCards(){
-            console.log("Abriendo creador de cartas");
-            const flashCardsCreatorTemlape = `
-            <div id="FlashCardCreatorPage" class="flashCardCreatorPage">
-                <div class="FCCreatorHeader">
-                    <img id="BackArrowIcon_CardsCreator" src="./SVG/chevron-left-gray.svg" alt="Back Arrow">
-                    <p class="subtitle">Flash Cards Creator</p>
-                    <p >0/0</p>
-                </div>
-                <form>
-                    <p>Front Part</p>
-                    <input id="FrontPartInput" class="inputFCCretor frontPartInput" type="text">
-                    <p>Back Part</p>
-                    <input id="BackPartInput" class="inputFCCretor backPartInput" type="text">
-                    <button class="done-button ">Crear FlashCard</button>
-                </form>
-            </div>`;
-            albumSlide.innerHTML += flashCardsCreatorTemlape;
+    function openAlbum(event){
+        const editIcon = document.querySelector(".editIcon");
+        const albumName = document.getElementById("albumName");
+        
     
-            const backArrowIcon_CardsCreator = document.getElementById("BackArrowIcon_CardsCreator");
-            backArrowIcon_CardsCreator.addEventListener("click", goBackToAlbum);
+        var albumNameValue = albumName.textContent;
     
-            function goBackToAlbum(){
-                console.log("Go back to the album");
-                
-                const albumSlideLastChild = albumSlide.lastChild;
-                if (albumSlideLastChild) {
-                    albumSlide.removeChild(albumSlideLastChild);
-                    assignEvLis();
-                }
+        if(event.target !== editIcon){
+            console.log("Abrindo album");
+    
+            function assignEvLis(){
+                const backArrowIcon_Album = document.getElementById("BackArrowIcon_Album");
+                const newFlashCardButton = document.getElementById("NewFlashCardButton");
+                backArrowIcon_Album.addEventListener("click", goBackToMain);
+                newFlashCardButton.addEventListener("click", createFlashCards);    
             }
-        } 
-    }  
+
+            const openAlbumTemplate =`
+            <div class="albumPage">
+                <div class="albumSlideHeader">
+                    <img id="BackArrowIcon_Album" class="backArrowIcon icon" src="./SVG/chevron-left-gray.svg" alt="back arrow">
+                    <h3 class="title-h2 titleAlbumSLide">${albumNameValue}</h3>
+                    <p id="cardsCounter" class="title-h2 cardsCounter">${counter}</p>
+                </div>
+                <article id="FlashCardsContainer" class="flashCardsContainer">
+
+                </article>
+            </div>
+            `;
+            albumSlide.innerHTML = openAlbumTemplate;
+
+            if (counter === 0 ){
+                const flashCardsContainer = document.getElementById("FlashCardsContainer")
+                const createFirstFCTemplate = `
+                    <div id="NewFlashCardButton" class="newFlashCardButton">
+                        <p class="subtitle">Crear cartas</p>
+                        <img src="./SVG/pus-Icon.svg" alt="plus icon">
+                    </div>
+                `;
+                flashCardsContainer.innerHTML = createFirstFCTemplate;
+                assignEvLis();
+            }
+            else {
+                asignCardsTemplate();
+            }
     
+            function goBackToMain(){
+                console.log("Cerrando album");
+                albumSlide.innerHTML = "";
+            } 
+            function createFlashCards(){
+                console.log("Abriendo creador de cartas");
+                const flashCardsCreatorTemlape = `
+                <div id="FlashCardCreatorPage" class="flashCardCreatorPage">
+                    <div class="FCCreatorHeader">
+                        <img id="BackArrowIcon_CardsCreator" class="icon" src="./SVG/chevron-left-gray.svg" alt="Back Arrow">
+                        <p class="title-h2">Flash Cards Creator</p>
+                        <p id="cardsMakerCounter" class="cardsCounter">0</p>
+                    </div>
+                    <form>
+                        <p>Front Part</p>
+                        <input id="FrontPartInput" class="inputFCCretor frontPartInput" type="text">
+                        <p>Back Part</p>
+                        <input id="BackPartInput" class="inputFCCretor backPartInput" type="text">
+                        <button type="button" id="CreateFlashCardsButton" class="done-button ">Guardar</button>
+                    </form>
+                    <span id="saveAnimation" class="">
+                        <img src="./SVG/check.svg"></img>
+                    </span>
+                </div>`;
+                albumSlide.innerHTML += flashCardsCreatorTemlape;
+        
+                const backArrowIcon_CardsCreator = document.getElementById("BackArrowIcon_CardsCreator");
+                const FrontPartInput = document.getElementById("FrontPartInput");
+                const BackPartInput = document.getElementById("BackPartInput");
+                const CreateFlashCardsButton = document.getElementById("CreateFlashCardsButton");
+                backArrowIcon_CardsCreator.addEventListener("click", goBackToAlbum);
+                CreateFlashCardsButton.addEventListener("click", saveNewFlashCards);
 
+                FrontPartInput.focus();
+
+                FrontPartInput.addEventListener("keydown", function(event) {
+                    if (event.key === "Enter") {
+                        event.preventDefault();
+                        BackPartInput.focus();
+                    }
+                });
+
+                BackPartInput.addEventListener("keydown", function(event) {
+                    if (event.key === "Enter" && FrontPartInput.value.trim() !== "") {
+                        event.preventDefault();
+                        CreateFlashCardsButton.click(); 
+                        FrontPartInput.focus(); 
+                    }
+                });
+
+                CreateFlashCardsButton.addEventListener("click", function() {
+                    const frontValue = FrontPartInput.value.trim();
+                    const backValue = BackPartInput.value.trim();
+
+                    if (frontValue !== "" && backValue !== "") {
+                        // Realiza aquí cualquier acción que deseas cuando se hace clic en el botón
+                        console.log("Botón 'Guardar' presionado con contenido en ambos campos.");
+                    } else {
+                        // Muestra un mensaje de error o realiza otra acción si los campos están vacíos
+                        console.log("Ambos campos deben tener contenido.");
+                    }
+                });
+
+
+                function goBackToAlbum(){
+                    console.log("Go back to the album");
+                    
+                    const albumSlideLastChild = albumSlide.lastChild;
+                    if (albumSlideLastChild) {
+                        albumSlide.removeChild(albumSlideLastChild);
+
+                        const backArrowIcon_Album = document.getElementById("BackArrowIcon_Album");
+                        backArrowIcon_Album.addEventListener("click", goBackToMain);
+
+                        if(counter === 0){
+                            const newFlashCardButton = document.getElementById("NewFlashCardButton");
+                            newFlashCardButton.addEventListener("click", createFlashCards);    
+                        }
+                    }
+                }
+                function saveNewFlashCards(){
+                    const saveAnimation = document.getElementById("saveAnimation");
+                    saveAnimation.classList.add("saveAnimation");
+
+                    setTimeout(() => {
+                        saveAnimation.classList.remove('saveAnimation');
+                    }, 1700);
+
+                    let FrontPartInputValue = FrontPartInput.value;
+                    let BackPartInputValue = BackPartInput.value;
+                    FrontPartInput.value = "";
+                    BackPartInput.value = "";
+
+                    arrayCards.push(
+                        {
+                            question: FrontPartInputValue,
+                            answer: BackPartInputValue
+                        }
+                    );
+                    
+                    console.log(arrayCards);
+
+                    asignCardsTemplate();
+            
+                    counterPlus();
+                    
+                }
+            } 
+        }  
+        
+    
+    }
 }
-
 
 
 albumCreatorContainer.addEventListener("click", closeOverlay);
